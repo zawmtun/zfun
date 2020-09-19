6986d95355edb819958d62d4b79de450471e3554
Zaw Myo Tun <zawmyotun11@gmail.com>
2020-09-19 17:34:57 +0800

    Change test from identical to equal.

--- tests/testthat/test-cumseq.R
+++ tests/testthat/test-cumseq.R
@@ -3,6 +3,6 @@ test_that("cumseq() create incremental sequence for a change in value", {
   b <- c(0, 0, 1, NA, NA)
   c <- c(1L, 1L, 2L, 3L, 3L)
 
-  expect_identical(cumseq(a), c)
-  expect_identical(cumseq(b, fill_missing = 9), c)
+  expect_equal(cumseq(a), c)
+  expect_equal(cumseq(b, fill_missing = 9), c)
 })
